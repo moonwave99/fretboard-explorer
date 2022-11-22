c1aa8ae545b24164b50eae51e351b3539cfe9ca0
Diego Caponera <diego.caponera@gmail.com>
2022-11-22 19:41:34 +0100

    Add drop3 voicings.

--- src/VoicingSystems/index.js
+++ src/VoicingSystems/index.js
@@ -1,17 +1,23 @@
-import * as Triads from "./triads";
-import * as Drop2 from "./drop2";
+import * as Triads from './triads';
+import * as Drop2 from './drop2';
+import * as Drop3 from './drop3';
 
 const systems = [
-  {
-    id: "triads",
-    title: "Triads",
-    ...Triads
-  },
-  {
-    id: "drop2",
-    title: "Drop 2 voicings",
-    ...Drop2
-  }
+    {
+        id: 'triads',
+        title: 'Triads',
+        ...Triads,
+    },
+    {
+        id: 'drop2',
+        title: 'Drop 2 voicings',
+        ...Drop2,
+    },
+    {
+        id: 'drop3',
+        title: 'Drop 3 voicings',
+        ...Drop3,
+    },
 ];
 
 export default systems;
